647ae9f953cd7a4c3708ff793619ddd83797c89c
Nastasia7777 <43110383+Nastasia7777@users.noreply.github.com>
2019-06-23 15:35:04 +0500

    Add files via upload

---
+++ Summa2/Summa2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Summa2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("input.txt");
+            StreamWriter sw = new StreamWriter("output.txt");
+            string number = sr.ReadToEnd();
+            sr.Close();
+            int x = Convert.ToInt32(number);
+            int result = 0;
+            for (int a = 1; a <= x - 3; a++)
+            {
+                for (int b = a; b <= x - a - 2; b++)
+                {
+                    for (int c = b; c <= x - a - b - 1; c++)
+                    {
+                        int d = x - a - b - c;
+                        if (c <= d)
+                        {
+                            result++;
+                        }
+                    }
+                }
+            }
+            sw.Write(result);
+            sw.Close();
+        }
+    }
+}
